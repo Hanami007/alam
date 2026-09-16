d1d4eca1553d0698a0f6cea5728a5eef7cc2e245
Maejanukidtung <jaroenyiw@gmail.com>
2026-09-16 14:59:16 +0700

    Yiw DB.2

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -41,6 +41,7 @@ export default function DynamicMemberProfilePage() {
             email: u.email,
             generation: u.generation,
             province: u.province,
+            work_province: u.workProvince || u.work_province,
             career_type: u.careerType || u.career_type,
             company: u.company,
             position: u.position,
